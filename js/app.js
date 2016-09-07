660f29b0e823a8d92dc060202bde68f53d4c9e93
Andrew <andrew.wilson@live.ca>
2016-09-07 13:20:11 -0400

    Basic Router Muli Page

--- js/app.js
+++ js/app.js
@@ -17,12 +17,13 @@ app.config(function($stateProvider, $urlRouterProvider) {
         // HOME STATES AND NESTED VIEWS ========================================
         .state('home', {
             url: '/home',
-            templateUrl: 'partial-home.html'
+            templateUrl: 'views/partial-home.html'
         })
         
         // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
         .state('about', {
-            // we'll get to this in a bit       
-        });
+            url: '/about',
+            templateUrl: 'views/partial-about.html'     
+        })
         
 });
