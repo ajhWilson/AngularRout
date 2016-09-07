6cb68816d59dfca556955a7ac52fb6ad181e5a58
Andrew <andrew.wilson@live.ca>
2016-09-07 15:22:11 -0400

    Basic Working template

--- js/app.js
+++ js/app.js
@@ -14,10 +14,19 @@ app.config(function($stateProvider, $urlRouterProvider) {
     
     $stateProvider
         
-        // HOME STATES AND NESTED VIEWS ========================================
+        // HOME STATES AND NESTED VIEWS 
         .state('home', {
             url: '/home',
             templateUrl: 'views/partial-home.html'
+        })
+
+        //nested list
+        .state('home.list',{
+          url: '/list',
+            templateUrl: 'views/partial-apples.html',
+            controller: function($scope) {
+            $scope.apples = ['Granny Smith', 'Red Delicious', 'Gala', 'Honeycrisp'];
+            }
         })
         
         // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
